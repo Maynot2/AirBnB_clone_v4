dc9e7c4b560c76f0d642829b5c100a54bc965dad
Maynot2 <paulmanot@gmail.com>
2021-05-13 14:49:23 +0000

    Fix indentent

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,27 +1,26 @@
-$(document).ready(function() {
-
+$(document).ready(function () {
   const list = [];
-  const amenitiesH4Elem = $(".amenities h4");
+  const amenitiesH4Elem = $('.amenities h4');
 
-  $(".check_box").click(function() {
-    const id = $(this).data("id");
+  $('.check_box').click(function () {
+    const id = $(this).data('id');
     if ($(this).is(':checked')) {
       if (list.length === 0) {
-        amenitiesH4Elem.append(($(this)).data("name"));
+        amenitiesH4Elem.append(($(this)).data('name'));
       } else {
-        amenitiesH4Elem.append(", " + ($(this)).data("name"));
+        amenitiesH4Elem.append(', ' + ($(this)).data('name'));
       }
       list.push(id);
     } else {
       const index = list.findIndex((el) => el === id);
       list.splice(index, 1);
       let newText;
-      if (index === 0 && list.length === 0) { 
-        newText = amenitiesH4Elem.text().replace(($(this)).data("name"), "");
+      if (index === 0 && list.length === 0) {
+        newText = amenitiesH4Elem.text().replace(($(this)).data('name'), '');
       } else if (index === 0) {
-        newText = amenitiesH4Elem.text().replace(($(this)).data("name") + ", ", "");
+        newText = amenitiesH4Elem.text().replace(($(this)).data('name') + ', ', '');
       } else {
-        newText = amenitiesH4Elem.text().replace(", " + ($(this)).data("name"), "");
+        newText = amenitiesH4Elem.text().replace(', ' + ($(this)).data('name'), '');
       }
       amenitiesH4Elem.text(newText);
     }
