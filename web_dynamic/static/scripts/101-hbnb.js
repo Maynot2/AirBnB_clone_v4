d2fe45c66dcd87ae9194c71c6ff371c59d5805ed
ThibautBernard <thibautbernard@sfr.fr>
2021-05-15 16:06:30 +0000

    fix && create: fix the 1-hbnb.html for the import missing a '?' && update 3-hbnb.js to give review for a place && add feature to show/hide reviews for a place && add some style to css for reviews

---
+++ web_dynamic/static/scripts/101-hbnb.js
@@ -0,0 +1,155 @@
+/**
+  Fetching places given amenities selection on event click
+  and fetch review for a place
+**/
+$(document).ready(function () {
+  const reviews = {};
+  const amenityList = [];
+  const amenitiesH4Elem = $('.amenities h4');
+
+  $('.amenities .check_box').click(function () {
+    const id = $(this).data('id');
+    if ($(this).is(':checked')) {
+      if (amenityList.length === 0) {
+        amenitiesH4Elem.text(($(this)).data('name'));
+      } else {
+        amenitiesH4Elem.append(', ' + ($(this)).data('name'));
+      }
+      amenityList.push(id);
+    } else {
+      const index = amenityList.findIndex((el) => el === id);
+      amenityList.splice(index, 1);
+      let newText;
+      if (index === 0 && amenityList.length === 0) {
+        amenitiesH4Elem.html('&nbsp;');
+        return;
+      } else if (index === 0) {
+        newText = amenitiesH4Elem.text().replace(($(this)).data('name') + ', ', '');
+      } else {
+        newText = amenitiesH4Elem.text().replace(', ' + ($(this)).data('name'), '');
+      }
+      amenitiesH4Elem.text(newText);
+    }
+  });
+
+  const stateList = [];
+  const stateH4Elem = $('.locations h4');
+
+  $('.state').click(function () {
+    const id = $(this).data('id');
+    if ($(this).is(':checked')) {
+      if (stateList.length === 0) {
+        stateH4Elem.text(($(this)).data('name'));
+      } else {
+        stateH4Elem.append(', ' + ($(this)).data('name'));
+      }
+      stateList.push(id);
+    } else {
+      const index = stateList.findIndex((el) => el === id);
+      stateList.splice(index, 1);
+      let newText;
+      if (index === 0 && stateList.length === 0) {
+        stateH4Elem.html('&nbsp;');
+        return;
+      } else if (index === 0) {
+        newText = stateH4Elem.text().replace(($(this)).data('name') + ', ', '');
+      } else {
+        newText = stateH4Elem.text().replace(', ' + ($(this)).data('name'), '');
+      }
+      stateH4Elem.text(newText);
+    }
+  });
+
+  const cityList = [];
+
+  $('.city').click(function () {
+    const id = $(this).data('id');
+    if ($(this).is(':checked')) {
+      if (stateList.length === 0 && cityList.length === 0) {
+        stateH4Elem.text(($(this)).data('name'));
+      } else {
+        stateH4Elem.append(', ' + ($(this)).data('name'));
+      }
+      cityList.push(id);
+    } else {
+      const index = cityList.findIndex((el) => el === id);
+      cityList.splice(index, 1);
+      let newText;
+      if (index === 0 && stateList.length === 0 && cityList.length === 0) {
+        stateH4Elem.html('&nbsp;');
+        return;
+      } else if (index === 0 && cityList.length > 1) {
+        newText = stateH4Elem.text().replace(($(this)).data('name') + ', ', '');
+      } else {
+        newText = stateH4Elem.text().replace(', ' + ($(this)).data('name'), '');
+      }
+      stateH4Elem.text(newText);
+    }
+  });
+
+  $('.filters button').click(() => {
+    $('.places').empty();
+    $.ajax({
+      type: 'POST',
+      url: 'http://0.0.0.0:5001/api/v1/places_search',
+      data: JSON.stringify({ amenities: amenityList, states: stateList, cities: cityList }),
+      contentType: 'application/json',
+      crossDomain: true,
+      dataType: 'json',
+      success: (places) => {
+        for (const place of places) {
+          const article =
+            `<article>
+               <div class="title_box">
+                 <h2>${place.name}</h2>
+                 <div class="price_by_night">$${place.price_by_night}</div>
+               </div>
+               <div class="information">
+                 <div class="max_guest">${place.max_guest} Guest${place.max_guest === 1 ? '' : 's'}</div>
+                 <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms === 1 ? '' : 's'}</div>
+                 <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms === 1 ? '' : 's'}</div>
+               </div>
+               <div class="user"></div>
+               <div class="description">${place.description}</div>
+               <div class="reviews">
+                  <div class="title">
+                    <h2 class="article_subtitle">Reviews</h2>
+                    <span data-id=${place.id}>show</span>
+                  </div>
+                  <div class="content">
+                    <ul class=${place.id}>
+                    </ul>
+                  </div>
+               </div>
+             </article>`;
+          $('.places').append(article);
+        }
+      }
+    }).done(() => {
+      $('.reviews span').click(function () {
+        const placeId = $(this).data('id');
+        const ulReview = $('.content .' + placeId);
+        // check that review for this specific place is not already printed
+        if (!reviews[placeId]) {
+          reviews[placeId] = true;
+          $.getJSON(`http://0.0.0.0:5001/api/v1/places/${placeId}/reviews`, (reviews) => {
+            for (const review of reviews) {
+              // get information of the user for this review
+              $.getJSON(`http://0.0.0.0:5001/api/v1/users/${review.user_id}`, (user) => {
+                const date = new Date(review.created_at)
+                const monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
+                                  'July', 'August', 'September', 'October', 'November', 'December']; 
+                ulReview.append(`<li> <b>From ${user.last_name} ${user.first_name} the ${date.getDay()}th ${monthNames[date.getMonth()]} ${date.getFullYear()} </b></li>`);
+                ulReview.append(`<li> ${review.text}</li>`);
+              });
+            }
+          });
+        // otherwise, delete the placeId from the reviews object and remove reviews from html
+        } else {
+          delete reviews[placeId];
+          ulReview.empty();
+        }
+      })
+    });
+  });
+});
--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,7 +1,9 @@
 /**
   Fetching all places with a post request
 **/
+
 $(document).ready(function () {
+  let reviews = {}
   $.ajax({
     type: 'POST',
     url: 'http://0.0.0.0:5001/api/v1/places_search',
@@ -11,7 +13,8 @@ $(document).ready(function () {
     dataType: 'json',
     success: (places) => {
       for (const place of places) {
-        const article =
+        // $.getJSON(`http://0.0.0.0:5001/api/v1/places/${place.id}/reviews`, (reviews) => 
+          const article =
             `<article>
                <div class="title_box">
                  <h2>${place.name}</h2>
@@ -24,10 +27,44 @@ $(document).ready(function () {
                </div>
                <div class="user"></div>
                <div class="description">${place.description}</div>
+               <div class="reviews">
+                  <div class="title">
+                    <h2 class="article_subtitle">Reviews</h2>
+                    <span data-id=${ place.id }>show</span>
+                  </div>
+                  <div class="content">
+                    <ul class=${ place.id }>
+                    </ul>
+                  </div>
+               </div>
              </article>`;
-
-        $('.places').append(article);
+          $('.places').append(article);
       }
     }
+  }).done(() => {
+    $('.reviews span').click(function () {
+        const placeId = $(this).data('id');
+        const ulReview = $('.content .' + placeId);
+        // check that review for this specific place is not already printed
+        if (!reviews[placeId]) {
+          reviews[placeId] = true;
+          $.getJSON(`http://0.0.0.0:5001/api/v1/places/${placeId}/reviews`, (reviews) => {
+            for (const review of reviews) {
+              // get information of the user for this review
+              $.getJSON(`http://0.0.0.0:5001/api/v1/users/${review.user_id}`, (user) => {
+                const date = new Date(review.created_at)
+                const monthNames = ["January", "February", "March", "April", "May", "June",
+                                  "July", "August", "September", "October", "November", "December"]; 
+                ulReview.append(`<li> <b>From ${user.last_name} ${user.first_name} the ${date.getDay()}th ${monthNames[date.getMonth()]} ${date.getFullYear()} </b></li>`);
+                ulReview.append(`<li> ${review.text}</li>`);
+              });
+            }
+          });
+        // otherwise, delete from the object and remove reviews from html
+        } else {
+          delete reviews[placeId];
+          ulReview.empty();
+        }
+      })
   });
 });
